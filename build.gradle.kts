7fdaae142b52750a1d407e21489019fb133c6425
Luna <62033805+Luna5ama@users.noreply.github.com>
2023-01-27 15:59:53 -0500

    Workspace setup

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    kotlin("jvm") version "1.8.0"
+    id("dev.fastmc.maven-repo").version("1.0.0")
+}
+
+group = "dev.fastmc"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+rootProject.name = "gl-wrapper"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://maven.fastmc.dev/")
+    }
+
+    val kotlinVersion: String by settings
+
+    plugins {
+        id("org.jetbrains.kotlin.jvm").version(kotlinVersion)
+    }
+}
