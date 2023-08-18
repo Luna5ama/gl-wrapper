6747c6b021c2f535b6902d57eec917d30a410237
Luna <62033805+Luna5ama@users.noreply.github.com>
2023-08-18 04:51:38 -0400

    size bits for easy comparison

--- shared/src/main/kotlin/dev/luna5ama/glwrapper/impl/Interfaces.kt
+++ shared/src/main/kotlin/dev/luna5ama/glwrapper/impl/Interfaces.kt
@@ -20,16 +20,25 @@ interface IGLSized1D : IGLObject {
     val sizeX: Int
 
     val width get() = sizeX
+
+    val sizeBit: Long
+        get() = (0b001L shl 61) or (sizeX and 0xFF).toLong()
 }
 
 interface IGLSized2D : IGLSized1D {
     val sizeY: Int
 
     val height get() = sizeY
+
+    override val sizeBit: Long
+        get() = (0b010L shl 61) or ((sizeY and 0xFF).toLong() shl 16) or (sizeX and 0xFF).toLong()
 }
 
 interface IGLSized3D : IGLSized2D {
     val sizeZ: Int
 
     val depth get() = sizeZ
+
+    override val sizeBit: Long
+        get() = (0b100L shl 61) or ((sizeZ and 0xFF).toLong() shl 32) or ((sizeY and 0xFF).toLong() shl 16) or (sizeX and 0xFF).toLong()
 }
