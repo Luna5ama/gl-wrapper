bcee18e7725df4753cb25ab73ed6bd1f165770a3
Luna <62033805+Luna5ama@users.noreply.github.com>
2024-08-21 22:32:56 -0400

    MC 1.16.5, 1.18.2, 1.19.4 support

--- architectury-1.18.2/common/src/main/kotlin/dev/luna5ama/glwrapper/api/GL11Mc.kt
+++ architectury-1.18.2/common/src/main/kotlin/dev/luna5ama/glwrapper/api/GL11Mc.kt
@@ -1,62 +1,85 @@
 package dev.luna5ama.glwrapper.api
 
 import com.mojang.blaze3d.platform.GlStateManager
+import com.mojang.blaze3d.systems.RenderSystem
 import org.lwjgl.opengl.GL11C
 
 class GL11Mc(private val delegate: GL11) : GL11 by delegate {
     override fun glEnable(target: Int) {
         when (target) {
             GL_SCISSOR_TEST -> GlStateManager._enableScissorTest()
-            GL_DEPTH_TEST -> GlStateManager._enableDepthTest()
-            GL_BLEND -> GlStateManager._enableBlend()
-            GL_CULL_FACE -> GlStateManager._enableCull()
-            GL_POLYGON_OFFSET_FILL -> GlStateManager._enablePolygonOffset()
-            GL_COLOR_LOGIC_OP -> GlStateManager._enableColorLogicOp()
-            GL_TEXTURE_2D -> GlStateManager._enableTexture()
+            GL_DEPTH_TEST -> RenderSystem.enableDepthTest()
+            GL_BLEND -> RenderSystem.enableBlend()
+            GL_CULL_FACE -> RenderSystem.enableCull()
+            GL_POLYGON_OFFSET_FILL -> RenderSystem.enablePolygonOffset()
+            GL_COLOR_LOGIC_OP -> RenderSystem.enableColorLogicOp()
             else -> delegate.glEnable(target)
         }
     }
 
     override fun glDisable(target: Int) {
         when (target) {
-            GL_SCISSOR_TEST -> GlStateManager._disableScissorTest()
-            GL_DEPTH_TEST -> GlStateManager._disableDepthTest()
-            GL_BLEND -> GlStateManager._disableBlend()
-            GL_CULL_FACE -> GlStateManager._disableCull()
-            GL_POLYGON_OFFSET_FILL -> GlStateManager._disablePolygonOffset()
-            GL_COLOR_LOGIC_OP -> GlStateManager._disableColorLogicOp()
-            GL_TEXTURE_2D -> GlStateManager._disableTexture()
+            GL_SCISSOR_TEST -> RenderSystem.disableScissor()
+            GL_DEPTH_TEST -> RenderSystem.disableDepthTest()
+            GL_BLEND -> RenderSystem.disableBlend()
+            GL_CULL_FACE -> RenderSystem.disableCull()
+            GL_POLYGON_OFFSET_FILL -> RenderSystem.disablePolygonOffset()
+            GL_COLOR_LOGIC_OP -> RenderSystem.disableColorLogicOp()
             else -> delegate.glDisable(target)
         }
     }
 
     override fun glBlendFunc(sfactor: Int, dfactor: Int) {
-        GlStateManager._blendFunc(sfactor, dfactor)
+        RenderSystem.blendFunc(sfactor, dfactor)
     }
 
     override fun glClearColor(red: Float, green: Float, blue: Float, alpha: Float) {
-        GlStateManager._clearColor(red, green, blue, alpha)
+        RenderSystem.clearColor(red, green, blue, alpha)
     }
 
     override fun glClearDepth(depth: Double) {
-        GlStateManager._clearDepth(depth)
+        RenderSystem.clearDepth(depth)
     }
 
     override fun glColorMask(red: Boolean, green: Boolean, blue: Boolean, alpha: Boolean) {
-        GlStateManager._colorMask(red, green, blue, alpha)
+        RenderSystem.colorMask(red, green, blue, alpha)
     }
 
     override fun glDepthFunc(func: Int) {
-        GlStateManager._depthFunc(func)
+        RenderSystem.depthFunc(func)
     }
 
     override fun glDepthMask(flag: Boolean) {
-        GlStateManager._depthMask(flag)
+        RenderSystem.depthMask(flag)
+    }
+
+    override fun glPolygonMode(face: Int, mode: Int) {
+        RenderSystem.polygonMode(face, mode)
+    }
+
+    override fun glPolygonOffset(factor: Float, units: Float) {
+        RenderSystem.polygonOffset(factor, units)
+    }
+
+    override fun glLogicOp(op: Int) {
+        GlStateManager._logicOp(op)
+    }
+
+    override fun glViewport(x: Int, y: Int, w: Int, h: Int) {
+        RenderSystem.viewport(x, y, w, h)
+    }
+
+    override fun glStencilFunc(func: Int, ref: Int, mask: Int) {
+        RenderSystem.stencilFunc(func, ref, mask)
+    }
+
+    override fun glStencilOp(sfail: Int, dpfail: Int, dppass: Int) {
+        RenderSystem.stencilOp(sfail, dpfail, dppass)
     }
 
     override fun glBindTexture(target: Int, texture: Int) {
         if (target == GL11C.GL_TEXTURE_2D) {
-            GlStateManager._bindTexture(texture)
+            RenderSystem.bindTexture(texture)
             return
         }
         delegate.glBindTexture(target, texture)
